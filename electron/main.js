5ecae67bb0b2bf98fefb508fff44b8888347f96d
André de Lima Michalsky <andre.michalsky12@gmail.com>
2026-08-24 19:35:07 -0300

    chore: initialize React and Electron

---
+++ electron/main.js
@@ -0,0 +1,49 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    show: false,
+
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (app.isPackaged) {
+    mainWindow.loadFile(
+      path.join(__dirname, "../dist/index.html")
+    );
+  } else {
+    mainWindow.loadURL("http://localhost:5173");
+  }
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
